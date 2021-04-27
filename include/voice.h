6ab1cea8dc303f33eaefccfc72c0892584d293e5
Ygreneb <ygreneb@benson.uber.space>
2021-04-27 16:39:29 +0200

    Changed calculation of tuning to offset-based with LUT

--- include/voice.h
+++ include/voice.h
@@ -40,6 +40,21 @@ enum VoiceParameters
 	kNumParameters = 1
 };
 
+//class VoiceStaticsOnce;
+
+class VoiceStatics
+{
+	friend class VoiceStaticsOnce;
+
+public:
+	static double getPythagoreanOffset(int32 pitch, int32 rootPitch);
+	static double getWerckmeisterIIIOffset(int32 pitch, int32 rootPitch);
+
+private:
+	static double pythagoreanOffsets[12]; // in Cents
+	static double werckmeisterIIIOffsets[12]; // in Cents
+};
+
 template<class SamplePrecision>
 class Voice : public Vst::VoiceBase<kNumParameters, SamplePrecision, 2, GlobalParameterState>
 {
@@ -82,84 +97,108 @@ void Voice<SamplePrecision>::noteOn(int32 pitch, ParamValue velocity, float tuni
 {
 	volume = globalParameters->volume;
 
-	freq = 440.0 * pow(2.0, (pitch - 69.0) / 12.0); // Equal tuning based on pitch
-	
-	//freq = freq * pow(2.0, tuning / 1200.0); // Update frequency based on MIDI event tuning (1.f = 1 Cent)
-	
+	freq = 440.0 * pow(2.0, (pitch - 69.0) / 12.0); // Equal step tuning based on pitch
+
 	if (globalParameters->tuning > 0.33) // Not equal step tuning, frequency needs update
 	{
 		int32 rootNotePitch = 60 + round(globalParameters->rootNote * 11.0); // 60 = MIDI pitch of Middle C
-		double rootNoteFreq = 440.0 * pow(2.0, (rootNotePitch - 69) / 12.0); // 69 = MIDI pitch of A = 440 Hz
-					
-		//if (pitchDelta != 0)
-		//{
-		//int32 numOfOctavesBtwn = trunc(pitchDelta / 12.0);
+		//double rootNoteFreq = 440.0 * pow(2.0, (rootNotePitch - 69) / 12.0); // 69 = MIDI pitch of A = 440 Hz
+		double offsetCents = 0.0;
 
-		// Transpose pitch and find integer number of fiths and octaves between pitch and root note
-		int32 pitchDelta = pitch - rootNotePitch;
-		int32 numOctavesBtwn;
-		int32 numFifthsBtwn;
+		if (globalParameters->tuning < 0.66)
+			offsetCents = VoiceStatics::getPythagoreanOffset(pitch, rootNotePitch);
+		else
+			offsetCents = VoiceStatics::getWerckmeisterIIIOffset(pitch, rootNotePitch);
 
-		for (int32 i = -6; i <= 6; ++i)
-		{
-			double numOctavesFlt;
-			if (abs(modf((7.0 * i - pitchDelta) / 12.0, &numOctavesFlt)) < 0.01)
-			{
-				numFifthsBtwn = i;
-				numOctavesBtwn = numOctavesFlt;
-				break;
-			}
-		}
-
-		//if (globalParameters->tuning < 0.66)
-		//{
-		// Pythagorean tuning
-		if (globalParameters->tuning < 0.66 && abs(numFifthsBtwn) == 6)
-		{
-			// This note is not playable in pythagorean tuning because not uniquely identifiable
-			freq = -1.0; 
-			return;
-		}
-
-		freq = rootNoteFreq * pow(3.0 / 2.0, numFifthsBtwn) * pow(2.0, -numOctavesBtwn);
-		//}
-		//else
-
-		if (globalParameters->tuning > 0.66)
-		{
-			// Werckmeister III tuning
-			// based on Pythagorean Tuning
-			double centOffset = 0.0;
-			if (numFifthsBtwn > 0)
-			{
-				static const double pythagoreanComma = 1200.0 * log2(pow(3.0 / 2.0, 12.0) / pow(2.0, 7.0));
-				if (numFifthsBtwn <= 3)
-				{
-					centOffset = numFifthsBtwn * 0.25 * pythagoreanComma;
-				}
-				else if (numFifthsBtwn >= 6)
-				{
-					centOffset = pythagoreanComma;
-				}
-				else
-				{
-					centOffset = 3 * 0.25 * pythagoreanComma;
-				}
-			}
-
-			if (centOffset != 0.0)
-			{
-				freq *= pow(2.0, centOffset / 1200.0);
-			}
-		}
-		//}
-
-			
-	
+		freq *= pow(2.0, offsetCents / 1200.0);
 	}
 
 	Vst::VoiceBase<kNumParameters, SamplePrecision, 2, GlobalParameterState>::noteOn(pitch, velocity, tuning, sampleOffset, noteId);
 }
+
+//template<class SamplePrecision>
+//void Voice<SamplePrecision>::noteOn(int32 pitch, ParamValue velocity, float tuning, int32 sampleOffset, int32 noteId)
+//{
+//	volume = globalParameters->volume;
+//
+//	freq = 440.0 * pow(2.0, (pitch - 69.0) / 12.0); // Equal tuning based on pitch
+//	
+//	//freq = freq * pow(2.0, tuning / 1200.0); // Update frequency based on MIDI event tuning (1.f = 1 Cent)
+//	
+//	if (globalParameters->tuning > 0.33) // Not equal step tuning, frequency needs update
+//	{
+//		int32 rootNotePitch = 60 + round(globalParameters->rootNote * 11.0); // 60 = MIDI pitch of Middle C
+//		double rootNoteFreq = 440.0 * pow(2.0, (rootNotePitch - 69) / 12.0); // 69 = MIDI pitch of A = 440 Hz
+//					
+//		//if (pitchDelta != 0)
+//		//{
+//		//int32 numOfOctavesBtwn = trunc(pitchDelta / 12.0);
+//
+//		// Transpose pitch and find integer number of fiths and octaves between pitch and root note
+//		int32 pitchDelta = pitch - rootNotePitch;
+//		int32 numOctavesBtwn;
+//		int32 numFifthsBtwn;
+//
+//		for (int32 i = -6; i <= 6; ++i)
+//		{
+//			double numOctavesFlt;
+//			if (abs(modf((7.0 * i - pitchDelta) / 12.0, &numOctavesFlt)) < 0.01)
+//			{
+//				numFifthsBtwn = i;
+//				numOctavesBtwn = numOctavesFlt;
+//				break;
+//			}
+//		}
+//
+//		//if (globalParameters->tuning < 0.66)
+//		//{
+//		// Pythagorean tuning
+//		if (globalParameters->tuning < 0.66 && abs(numFifthsBtwn) == 6)
+//		{
+//			// This note is not playable in pythagorean tuning because not uniquely identifiable
+//			freq = -1.0; 
+//			return;
+//		}
+//
+//		freq = rootNoteFreq * pow(3.0 / 2.0, numFifthsBtwn) * pow(2.0, -numOctavesBtwn);
+//		//}
+//		//else
+//
+//		if (globalParameters->tuning > 0.66)
+//		{
+//			// Werckmeister III tuning
+//			// based on Pythagorean Tuning
+//			double centOffset = 0.0;
+//			if (numFifthsBtwn > 0)
+//			{
+//				static const double pythagoreanComma = 1200.0 * log2(pow(3.0 / 2.0, 12.0) / pow(2.0, 7.0));
+//				if (numFifthsBtwn <= 3)
+//				{
+//					centOffset = numFifthsBtwn * 0.25 * pythagoreanComma;
+//				}
+//				else if (numFifthsBtwn >= 6)
+//				{
+//					centOffset = pythagoreanComma;
+//				}
+//				else
+//				{
+//					centOffset = 3 * 0.25 * pythagoreanComma;
+//				}
+//			}
+//
+//			if (centOffset != 0.0)
+//			{
+//				freq *= pow(2.0, centOffset / 1200.0);
+//			}
+//		}
+//		//}
+//
+//			
+//	
+//	}
+//
+//	Vst::VoiceBase<kNumParameters, SamplePrecision, 2, GlobalParameterState>::noteOn(pitch, velocity, tuning, sampleOffset, noteId);
+//}
 
 template<class SamplePrecision>
 void Voice<SamplePrecision>::noteOff(ParamValue velocity, int32 sampleOffset)
--- source/voice.cpp
+++ source/voice.cpp
@@ -77,5 +77,58 @@ tresult GlobalParameterState::getState(IBStream* stream)
 	return kResultTrue;
 }
 
+double VoiceStatics::pythagoreanOffsets[12];
+double VoiceStatics::werckmeisterIIIOffsets[12];
+
+double VoiceStatics::getPythagoreanOffset(int32 pitch, int32 rootPitch)
+{
+	int32 pitchMod = (pitch - rootPitch) % 12;
+	//double octaves = trunc((double)pitch - rootPitch);
+	return pythagoreanOffsets[pitchMod];// +octaves * 1200.0;
+}
+
+double VoiceStatics::getWerckmeisterIIIOffset(int32 pitch, int32 rootPitch)
+{
+	int32 pitchMod = (pitch - rootPitch) % 12;
+	//double octaves = trunc((double)pitch - rootPitch);
+	return werckmeisterIIIOffsets[pitchMod];// +octaves * 1200.0;
+}
+
+class VoiceStaticsOnce
+{
+public:
+	VoiceStaticsOnce()
+	{
+		static const double pComma = 1200.0 * log2(pow(1.5, 12.0) / pow(2.0, 7.0)); // in Cents
+
+		//VoiceStatics::pythagoreanOffsets[0] = 0.0;																// unison
+		//VoiceStatics::pythagoreanOffsets[1] = 1200.0 * log2( pow(3.0 / 2.0, -5.0) * pow(2.0,  3.0) ) - 100.0;	// minor second
+		//VoiceStatics::pythagoreanOffsets[2] = 1200.0 * log2( pow(3.0 / 2.0,  2.0) * 0.5			   ) - 200.0;	// major second
+		//VoiceStatics::pythagoreanOffsets[3] = 1200.0 * log2( pow(3.0 / 2.0, -3.0) * 4.0            ) - 300.0;	// minor third
+		//VoiceStatics::pythagoreanOffsets[4] = 1200.0 * log2( pow(3.0 / 2.0,  4.0) * 1.0 / 4.0	   ) - 400.0;	// major third
+		//VoiceStatics::pythagoreanOffsets[5] = 1200.0 * log2( 4.0 / 3.0							   ) - 500.0;	// fourth
+		//VoiceStatics::pythagoreanOffsets[6] = 0.0;																// augmented fourth / diminished fifth => undefined
+		//VoiceStatics::pythagoreanOffsets[7] = 1200.0 * log2( 3.0 / 2.0							   ) - 700.0;	// fifth
+		//VoiceStatics::pythagoreanOffsets[8] = 1200.0 * log2( pow(3.0 / 2.0, -4.0) * pow(2.0,  3.0) ) - 800.0;	// minor sixth
+		//VoiceStatics::pythagoreanOffsets[9] = 1200.0 * log2( pow(3.0 / 2.0,  3.0) * 0.5			   ) - 900.0;	// major sixth
+		//VoiceStatics::pythagoreanOffsets[9] = 1200.0 * log2( pow(3.0 / 2.0, -2.0) * 4.0			   ) - 1000.0;	// minor seventh
+		//VoiceStatics::pythagoreanOffsets[9] = 1200.0 * log2( pow(3.0 / 2.0,  5.0) * 1.0 / 4.0      ) - 1100.0;	// major seventh
+
+		for (int i = 0; i < 12; ++i)
+		{
+			const int numFifths = (i * 7 + 5) % 12 - 5; // in range [-5:6]
+			const int numOctaves = (i * 3 + 3) % 7 - 3; // in range [-3:3]
+			VoiceStatics::pythagoreanOffsets[i] = 1200.0 * log2(pow(1.5, numFifths) * pow(2.0, numOctaves)) - i * 100.0;
+			VoiceStatics::werckmeisterIIIOffsets[i] = VoiceStatics::pythagoreanOffsets[i];
+			if (numFifths >= 1 && numFifths <= 3)
+				VoiceStatics::werckmeisterIIIOffsets[i] -= 0.25 * pComma * numFifths;
+			else if (numFifths == 6)
+				VoiceStatics::werckmeisterIIIOffsets[i] -= pComma;
+		}
+	}
+};
+
+static VoiceStaticsOnce _gVoiceStaticsOnce;
+
 }
 }
